2914a5a80f7ba588a12f8bcbdddcb2c7973e0eea
Steven! Ragnarök <steven@nuclearsandwich.com>
2012-01-30 22:26:44 -0800

    Initial commit.

---
+++ README.md
@@ -0,0 +1,14 @@
+The Land of Lisp
+================
+
+*My journey through the Land of Lisp. Guided by Conrad Barski*
+
+This is a collection, initially of games from the book [The Land of Lisp][1] but
+I'll also be using this as my noodling repo for GNU CLISP and SBCL code.
+
+Where I remember I note that the games come from the book and my say something
+about a specific lesson or computer science concept the game showcases, but
+really if you like what you see you should get Conrad's book.
+
+[1]: http://nostarch.com/lisp.htm
+
---
+++ guess-my-number.lisp
@@ -0,0 +1,29 @@
+;;;;;;;;;;;;;;;;;;;;;;
+;; Guess The Number ;;
+;;;;;;;;;;;;;;;;;;;;;;
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+; From Conrad Barski's Land of Lisp ;
+;    http://nostarch.com/lisp.htm   ;
+;      Showcasing Binary Search     ;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(defparameter *big* 100)
+
+(defparameter *small* 1)
+
+(defun guess-my-number ()
+  (ash (+ *small* *big*) -1))
+
+(defun smaller ()
+  (setf *big* (1- (guess-my-number)))
+  (guess-my-number))
+
+(defun bigger ()
+  (setf *small* (1+ (guess-my-number)))
+  (guess-my-number))
+
+(defun start-over ()
+  (defparameter *small* 1)
+  (defparameter *big* 100))
+
